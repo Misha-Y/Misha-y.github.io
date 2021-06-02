a948a1eef240e3ca7b771358add5e7800592811c
Misha-Y <44838450+Misha-Y@users.noreply.github.com>
2021-06-02 22:37:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    callBack = document.quarySelector('.callback'),
+    btn = document.querySelectorAll('button');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -12,6 +14,16 @@ window.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
-        })
-    })
+        });
+    });
+
+    modal.forEach(btn => {
+        if(btn.target !== callBack){
+        btn.addEventListener('click', () => {
+            btn.classList.style.add('show');
+            btn.classList.style.remove('hidden');
+            document.body.style.overflow = '';
+        });
+        }
+    });
 })
